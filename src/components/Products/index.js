5331f5b88f9a7b79b5cf297a9919d2d86986c267
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-05-27 21:45:07 +0000

    Adicionando Produtos

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import {
+    ProductsContainer,
+    ProductWrapper,
+    ProductsHeading,
+    ProductCard,
+    ProductImg,
+    ProductInfo,
+    ProductTitle,
+    ProductDesc,
+    ProductPrice,
+    ProductButton   
+} from './ProductsElements';
 
-const Products = () => {
+const Products = ({ heading, data }) => {
     return (
         <ProductsContainer>
-            <ProductsHeading>heading</ProductsHeading>
+            <ProductsHeading>{heading}</ProductsHeading>
             <ProductWrapper>
-                {data.map}
+                {data.map((product, index) => {
+                    return (
+                        <ProductCard key={index}>
+                            <ProductImg src={product.img} alt={product.alt} />
+                            <ProductInfo>
+                                <ProductTitle>{product.name}</ProductTitle>
+                                <ProductDesc>{product.desc}</ProductDesc>
+                                <ProductPrice>{product.price}</ProductPrice>
+                                <ProductButton>{product.button}</ProductButton>
+                            </ProductInfo>
+                        </ProductCard>
+                    )
+                })}
             </ProductWrapper>
         </ProductsContainer>
     )
