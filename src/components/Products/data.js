ecdc76c7e278add5a65f273b711b6e9dbbc6ad85
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-05-27 21:52:27 +0000

    Corrigindo moeda cardapio

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -8,7 +8,7 @@ export const productData = [
         alt: 'Pizza',
         name: 'Supreme Pizza',
         desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '$19.99',
+        price: '€19.99',
         button: 'Add to Cart'
     },
     {
@@ -16,7 +16,7 @@ export const productData = [
         alt: 'Pizza',
         name: 'Hawaiian Paradise',
         desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '$16.99',
+        price: '€16.99',
         button: 'Add to Cart'
     },
     {
@@ -24,7 +24,7 @@ export const productData = [
         alt: 'Pizza',
         name: 'Veggie Overload',
         desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '$14.99',
+        price: '€14.99',
         button: 'Add to Cart'
     }
 ]
