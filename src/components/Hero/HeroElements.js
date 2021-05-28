5161509750e2d6e587309400ca1894798c3172a7
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-05-28 12:27:50 +0000

    Adicionando produtos no menu1

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -3,7 +3,8 @@ import ImgBg from '../../images/pizza-3.jpg';
 
 export const HeroContainer = styled.div`
     background: linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0,0.1)), url(${ImgBg});
-    height: auto;
+    width: 100vw;
+    padding: 5rem calc((100vh - 1300px) /2);
     background-position: center;
     background-size: cover;
 `
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,30 +1,30 @@
-import product1 from '../../images/product-1.jpg';
-import product2 from '../../images/product-2.jpg';
-import product3 from '../../images/product-3.jpg';
+import product1 from '../../images/Tears_for_Fears.jpg';
+import product2 from '../../images/Pet_Shop_Boys.jpg';
+import product3 from '../../images/Prince.jpg';
 
 export const productData = [
     {
         img: product1,
         alt: 'Pizza',
-        name: 'Supreme Pizza',
-        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '€19.99',
-        button: 'Add to Cart'
+        name: 'Tears for Fears',
+        desc: 'Mussarela, Tomate, Parmesão, Manjericão',
+        price: '€13,00',
+        button: 'Add to Cart',
     },
     {
         img: product2,
         alt: 'Pizza',
-        name: 'Hawaiian Paradise',
-        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '€16.99',
-        button: 'Add to Cart'
+        name: 'Pet Shop Boys',
+        desc: 'Queijo Mussarela',
+        price: '€10,00',
+        button: 'Add to Cart',
     },
     {
         img: product3,
         alt: 'Pizza',
-        name: 'Veggie Overload',
-        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-        price: '€14.99',
-        button: 'Add to Cart'
+        name: 'Prince',
+        desc: 'Mussarela, Fiambre, Ananás',
+        price: '€12,00',
+        button: 'Add to Cart',
     }
 ]
