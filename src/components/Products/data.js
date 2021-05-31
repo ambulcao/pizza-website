03bb0a0eaad96b03f09976994469a0d5151df332
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-05-31 21:00:01 +0000

    Adicionando produtos ao menu2

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from '../../images/Tears_for_Fears.jpg';
 import product2 from '../../images/Pet_Shop_Boys.jpg';
 import product3 from '../../images/Prince.jpg';
+import product4 from '../../images/LedZeppelin.jpg';
+import product5 from '../../images/PinkFloyd.jpg';
+import product6 from '../../images/TheCure.jpg';
 
 export const productData = [
     {
@@ -25,6 +28,30 @@ export const productData = [
         name: 'Prince',
         desc: 'Mussarela, Fiambre, Ananás',
         price: '€12,00',
+        button: 'Add to Cart',
+    },
+    {
+        img: product4,
+        alt: 'Pizza',
+        name: 'Led Zeppelin',
+        desc: 'Mussarela, Lombo Defumado, Bacon, Cebola',
+        price: '€14,00',
+        button: 'Add to Cart',
+    },
+    {
+        img: product5,
+        alt: 'Pizza',
+        name: 'Pink Floyd',
+        desc: 'Mussarela, Bacon, Ovos Picados, Tomate',
+        price: '€14,00',
+        button: 'Add to Cart',
+    },
+    {
+        img: product6,
+        alt: 'Pizza',
+        name: 'The Cure',
+        desc: 'Brócolis, Champignon, Pimento, Mussarela Salpicada',
+        price: '€13,00',
         button: 'Add to Cart',
     }
 ]
