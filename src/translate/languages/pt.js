495572b5ba00a03110770eddaa3d0ee64067890c
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-06-14 22:08:51 +0000

    Arquivos multilanguage

---
+++ src/translate/languages/pt.js
@@ -0,0 +1,18 @@
+const messages = {
+    pt: {
+        translations: {
+            titles: {
+                app: 'Pizzaria e Cervejaria' 
+            },
+            messages: {
+                itWorks: 'A melhor pizza de todos os tempos',
+                smallText: 'Pizza Favorita Gostosa'
+            },
+            buttons: {
+                save: 'Faça a encomenda'
+            },
+        }
+    }
+}
+
+export { messages }
