f789ebe0104109b01c96c0778471f7273da5423a
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-06-15 21:24:22 +0000

    Ajustando arquivos language

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import {
   productDataTree 
 } from './components/Products/data';
 import Feature from './components/Feature';
-import { i18n } from './translate/i18n';
 
 function App() {
   return (
@@ -21,17 +20,11 @@ function App() {
       <GlobalStyle/>
       <Hero/>
       <Products heading='Pizza Favorita 🇧🇷' data={productDataBr}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇧🇷')} data={productDataBr}/>*/}
       <Products heading='Pizza Favorita 🇵🇹' data={productDataPt}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇵🇹')} data={productDataPt}/>*/}
       <Products heading='Pizza Favorita 🇬🇧' data={productDataUK}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇬🇧')} data={productDataUK}/>*/}
       <Products heading='Pizza Favorita 🇺🇸' data={productDataEua}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇺🇸')} data={productDataEua}/>*/}
       <Products heading='Pizza Favorita 🇩🇪' data={productDataAle}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇩🇪')} data={productDataAle}/>*/}
       <Products heading='Pizza Favorita 🇪🇺' data={productDataUea}/>
-      {/*<Products heading={i18n.t('messages.smallText 🇪🇺')} data={productDataUea}/>*/}
       <Feature />
       <Products heading='Escolha sua Sobremessa' data={productDataTree}/>
     </Router>
